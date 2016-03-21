74eb061017276ab7f29f2fbe088ced36d5947746
JoshSutcliffe <joshclement12@gmail.com>
2016-03-21 14:20:28 +1100

    working on routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Chase page. */
+router.get('/chase', function(req, res) {
+    res.render('chase', { title: 'The Chase' });
+});
+
+/* GET Hunt page. */
+router.get('/hunt', function(req, res) {
+    res.render('hunt', { title: 'The Hunt!' });
+});
+
 module.exports = router;
